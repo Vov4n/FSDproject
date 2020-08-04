8e22601f3eae576dcd52bd59ad136eb3fb0bd43c
Vladimir <vovan-m@yandex.ru>
2020-08-04 17:42:01 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,85 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const loader = require('sass-loader');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: {
-        main: './index.js'
+        main: ['@babel/polyfill', './index.js']
     },
     output: {
         // filename: filename('js'),
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        port: 4200
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false,
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src'),
+                to: path.resolve(__dirname, 'dist')
+            }, ],
+            options: {
+                concurrency: 100,
+            },
+        }),
     ],
     module: {
         rules: [{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }]
-    }
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {},
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+
 };
